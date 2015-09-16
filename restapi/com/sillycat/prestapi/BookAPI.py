1f5547ade883dcf295f9fa913fb219493bf26f37
Carl Luo <cluo@jobs2careers.com>
2015-09-16 15:39:41 -0500

    final codes

---
+++ restapi/com/sillycat/prestapi/BookAPI.py
@@ -0,0 +1,27 @@
+from txrestapi.methods import GET, POST, PUT, ALL, DELETE
+from txrestapi.resource import APIResource
+
+class BookResource(APIResource):
+    @GET('^/book/(?P<id>[^/]+)')
+    def getBook(self, request, id):
+        return 'Pick up one book with id %s' % id
+
+    @GET('^/book')
+    def books(self,request):
+        return "books"
+
+    @PUT('^/book/(?P<id>[^/]+)')
+    def updateBook(self,request):
+        return "Update book with id %s" % id
+
+    @POST('^/book/(?P<id>[^/]+)')
+    def saveBook(self, request, id):
+        return "Save book with id %s" % id
+
+    @DELETE('^/book/(?P<id>[^/]+)')
+    def deleteBook(self,request,id):
+        return "Delete book with id %s" % id
+
+    @ALL('^/')
+    def default_view(self, request):
+        return "I fail to match other URLs."
---
+++ restapi/com/sillycat/prestapi/RestAPI.py
@@ -0,0 +1,15 @@
+from twisted.web.resource import Resource
+from twisted.web.server import Site
+from twisted.internet import reactor
+
+from BookAPI import BookResource
+
+bookResource = BookResource()
+
+
+rootResource = Resource()
+rootResource.putChild("book", bookResource)
+
+site = Site(rootResource, timeout=None)
+reactor.listenTCP(8888,site)
+reactor.run()
